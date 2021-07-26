4ebabec1b724ce1bdff440b7f630d183be2034d1
Amartya Bhattacharya <44440114+AmartyaCodes@users.noreply.github.com>
2021-07-26 21:10:31 +0530

    Add files via upload

---
+++ solution.js
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>File Validation-1</title>
+</head>
+
+<body>
+	
+<p>
+		<input type="file"
+			id="file"
+			onchange="Filevalidation()" />
+	</p>
+
+
+	<p id="size"></p>
+
+</body>
+
+<script>
+	Filevalidation = () => {
+		const fi = document.getElementById('file');
+		// Check if any file is selected.
+		if (fi.files.length > 0) {
+			for (const i = 0; i <= fi.files.length - 1; i++) {
+
+				const fsize = fi.files.item(i).size;
+				const file = Math.round((fsize / 1024));
+				// The size of the file.
+				if (file < 1024) {
+					alert(
+					"File too small, please select a file greater than 1mb");
+				} else {
+					document.getElementById('size').innerHTML = '<b>'
+					+ file + '</b> KB';
+				}
+			}
+		}
+	}
+</script>
+
+</html>
+
